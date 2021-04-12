c0cf70c90be4e666a207bbfcd24d859dc29f6810
Ingrid Vallecilla <stepha4424@gmail.com>
2021-04-11 21:23:46 -0500

    actualizacion en triangulo

--- src/modelo/Triangulo.java
+++ src/modelo/Triangulo.java
@@ -1,5 +1,18 @@
 package modelo;
 
-public class Triangulo {
+import processing.core.PApplet;
+
+public class Triangulo extends Figura{
+
+	public Triangulo(PApplet app, int tam, int posX, int posY, int dir, int valor) {
+		super(app, tam, posX, posY, dir, valor);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void pintar() {
+		// TODO Auto-generated method stub
+		getApp().triangle(getPosX()-getTam(), getPosY()+getTam(), getPosX(), getPosY()-getTam(), getPosX()+getTam(), getPosY()+getTam());
+	}
 
 }
