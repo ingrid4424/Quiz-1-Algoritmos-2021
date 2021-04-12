749090bc14a50c01be618d4ed7b885e9c4a47598
Ingrid Vallecilla <stepha4424@gmail.com>
2021-04-11 22:33:28 -0500

    funciona crear cuadrados

--- src/modelo/Logica.java
+++ src/modelo/Logica.java
@@ -48,7 +48,7 @@ public class Logica {
 			listTriangulos.get(i).mover();
 		}
 		if(this.app.frameCount > 120) {
-			//crearTriangulo();
+			crearTriangulo();
 		}
 			
 		
@@ -85,18 +85,19 @@ public class Logica {
 	
 	public void crearTriangulo() {
 		for(int i=0; i<listCirculos.size(); i++) {
-			for(int j=0; i<listCuadrados.size(); j++) {
+			for(int j=0; j<listCuadrados.size(); j++) {
 				if(listCirculos.get(i).getPosX()> listCuadrados.get(j).getPosX()
 						&& listCirculos.get(i).getPosX()< (listCuadrados.get(j).getPosX()+listCuadrados.get(j).getTam()) &&
 						listCirculos.get(i).getPosY()> listCuadrados.get(j).getPosY() && 
 						listCirculos.get(i).getPosY()< (listCuadrados.get(j).getPosY()+listCuadrados.get(j).getTam())	) {
 					
-					//int valor = listCirculos.get(i).getValor() +  listCirculos.get(j).getValor();
-					//listTriangulos.add(new Triangulo(this.app, (int) this.app.random(30,100),(int) this.app.random(100,500) , (int) this.app.random(100,500), (int) this.app.random(10), valor));
-					//listCirculos.remove(i);
-					//listCuadrados.remove(j);
+					int valor = listCirculos.get(i).getValor() +  listCuadrados.get(j).getValor();
+					listTriangulos.add(new Triangulo(this.app, (int) this.app.random(30,100),(int) this.app.random(100,500) , (int) this.app.random(100,500), (int) this.app.random(10), valor));
+					listCirculos.remove(i);
+					if(this.app.frameCount % 60 ==0) {
+					listCuadrados.remove(j);
 					
-					
+					}
 				}
 			}
 		}
--- src/modelo/Triangulo.java
+++ src/modelo/Triangulo.java
@@ -3,16 +3,24 @@ package modelo;
 import processing.core.PApplet;
 
 public class Triangulo extends Figura{
+	private int  r,g,b;
 
 	public Triangulo(PApplet app, int tam, int posX, int posY, int dir, int valor) {
 		super(app, tam, posX, posY, dir, valor);
 		// TODO Auto-generated constructor stub
+		
+		 r = (int) getApp().random(255);
+		 g = (int) getApp().random(255);
+		 b = (int) getApp().random(255);
 	}
 
 	@Override
 	public void pintar() {
 		// TODO Auto-generated method stub
+		this.getApp().fill(r,g,b);
 		getApp().triangle(getPosX()-getTam(), getPosY()+getTam(), getPosX(), getPosY()-getTam(), getPosX()+getTam(), getPosY()+getTam());
+		this.getApp().fill(0);
+		this.getApp().text(""+ getValor(), getPosX(), getPosY());
 	}
 
 }
